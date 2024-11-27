da24f662609f64cbcb0715f64b6fe7b7447198e3
Kieran Chambers <kieran.chambers@icloud.com>
2024-11-27 13:56:02 +0000

    Feat: Create views, routes and HTML files for website sections

--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
+
+
+def projects(request):
+    return render(request, 'home/projects.html')
