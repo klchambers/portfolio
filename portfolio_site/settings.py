a030fae48d372e2d5c492170d077d2053558c834
Kieran Chambers <kieran.chambers@icloud.com>
2025-01-09 20:59:37 -0500

    Feat: Display Django messages in base template to give user feedback, display them with Bootstrap alerts

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -14,6 +14,16 @@ from pathlib import Path
 import os
 if os.path.isfile('env.py'):
     import env # noqa
+from django.contrib.messages import constants as messages
+
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
